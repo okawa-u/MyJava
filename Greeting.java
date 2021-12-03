60b973df1c714d8a943f61141b186ad24f1e4b0b
Okawa <pandanyanya@gmail.com>
2021-12-03 09:35:27 +0900

    Initial commit

---
+++ Greeting.java
@@ -0,0 +1,10 @@
+class Greeting{
+  public static void main(String[] args){
+    System.out.println("Good morning");
+    // println=文字列や変数の中の参照
+
+    System.out.println("Good afternoon");
+
+    System.out.println("Good evening");
+  }
+}
---
+++ one.java
@@ -0,0 +1,10 @@
+class one {
+  public static void main(String[] args){
+    int var;
+
+    var=1;
+    // 変数の代入
+
+    System.out.println(var);
+  }
+}
